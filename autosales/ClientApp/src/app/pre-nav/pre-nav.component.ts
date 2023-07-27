8e89d464fcc3bb36b9f8fcbccbfde882e741571c
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2023-07-27 15:04:28 +0300

    pre nav component

---
+++ autosales/ClientApp/src/app/pre-nav/pre-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-nav',
+  templateUrl: './pre-nav.component.html',
+  styleUrls: ['./pre-nav.component.css']
+})
+export class PreNavComponent {
+  isLogin = false;
+}
