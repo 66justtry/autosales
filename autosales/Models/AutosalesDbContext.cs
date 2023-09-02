8f24b737ec42832191d47f0d709b29a518a4695f
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2023-09-02 20:58:52 +0300

    database classes + dbcontext

--- autosales/Models/AutosalesDbContext.cs
+++ autosales/Models/AutosalesDbContext.cs
@@ -12,6 +12,14 @@ namespace autosales.Models
         {
 
         }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Model> Models { get; set; }
+        public virtual DbSet<Usage> Usages { get; set; }
+        public virtual DbSet<Info> Infos { get; set; }
+        public virtual DbSet<Type> Types { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<State> States { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //to do: get connection string from json file
@@ -20,6 +28,99 @@ namespace autosales.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // to set setting of dbsets
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("car_pkey");
+                entity.ToTable("car");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Price).HasColumnName("price");
+                entity.Property(e => e.IsDeleted).HasColumnName("isdeleted");
+            });
+
+            modelBuilder.Entity<Creation>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("creation_pkey");
+                entity.ToTable("creation");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Year).HasColumnName("year");
+                entity.Property(e => e.CarId).HasColumnName("carid");
+                entity.Property(e => e.BrandId).HasColumnName("brandid");
+                entity.Property(e => e.ModelId).HasColumnName("modelid");
+                entity.HasOne(c => c.CarNavigation).WithOne(car => car.CreationNavigation).HasForeignKey<Creation>(c => c.CarId).HasConstraintName("creation_carid_fkey");
+                entity.HasOne(c => c.BrandNavigation).WithMany(brand => brand.CreationNavigation).HasForeignKey(c => c.BrandId).HasConstraintName("creation_brandid_fkey");
+                entity.HasOne(c => c.ModelNavigation).WithMany(model => model.CreationNavigation).HasForeignKey(c => c.ModelId).HasConstraintName("creation_modelid_fkey");
+            });
+
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("brand_pkey");
+                entity.ToTable("brand");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Model>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("model_pkey");
+                entity.ToTable("model");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Name).HasColumnName("name");
+                entity.HasOne(m => m.BrandNavigation).WithMany(b => b.ModelNavigation).HasForeignKey(m => m.BrandId).HasConstraintName("model_brandid_fkey");
+            });
+
+            modelBuilder.Entity<Usage>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("usage_pkey");
+                entity.ToTable("usage");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.IsNew).HasColumnName("isnew");
+                entity.Property(e => e.IsDamaged).HasColumnName("isdamaged");
+                entity.Property(e => e.Mileage).HasColumnName("mileage");
+                entity.Property(e => e.CarId).HasColumnName("carid");
+                entity.HasOne(u => u.CarNavigation).WithOne(c => c.UsageNavigation).HasForeignKey<Usage>(u => u.CarId).HasConstraintName("usage_carid_fkey");
+            });
+
+            modelBuilder.Entity<Info>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("info_pkey");
+                entity.ToTable("info");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Description).HasColumnName("description");
+                entity.Property(e => e.CarId).HasColumnName("carid");
+                entity.Property(e => e.TypeId).HasColumnName("typeid");
+                entity.Property(e => e.ColorId).HasColumnName("colorid");
+                entity.Property(e => e.StateId).HasColumnName("stateid");
+                entity.HasOne(i => i.CarNavigation).WithOne(c => c.InfoNavigation).HasForeignKey<Info>(i => i.CarId).HasConstraintName("info_carid_fkey");
+                entity.HasOne(i => i.TypeNavigation).WithMany(t => t.InfoNavigation).HasForeignKey(i => i.TypeId).HasConstraintName("info_typeid_fkey");
+                entity.HasOne(i => i.ColorNavigation).WithMany(c => c.InfoNavigation).HasForeignKey(i => i.ColorId).HasConstraintName("info_colorid_fkey");
+                entity.HasOne(i => i.StateNavigation).WithMany(s => s.InfoNavigation).HasForeignKey(i => i.StateId).HasConstraintName("info_stateid_fkey");
+            });
+
+            modelBuilder.Entity<Type>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("type_pkey");
+                entity.ToTable("type");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Color>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("color_pkey");
+                entity.ToTable("color");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+
+            modelBuilder.Entity<State>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("state_pkey");
+                entity.ToTable("state");
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnName("id");
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
--- autosales/Models/Car.cs
+++ autosales/Models/Car.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int Price { get; set; }
+        public bool IsDeleted { get; set; }
         public virtual Creation? CreationNavigation { get; set; }
         public virtual Usage? UsageNavigation { get; set; }
         public virtual Info? InfoNavigation { get; set; }
